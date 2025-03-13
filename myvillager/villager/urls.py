19f7eb7dbdf23b5a28670acf63357574d0acb455
mohamad-javad-jokar-sarhangi <mjavadx3@gmail.com>
2025-03-13 13:26:19 +0330

    add villagers urls

--- myvillager/villager/urls.py
+++ myvillager/villager/urls.py
@@ -1,1 +1,14 @@
-from .models import Villager
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VillagerViewSet, DriverViewSet, LeaderViewSet, SellerViewSet
+
+# ایجاد Router برای ViewSetهای ثبت‌شده
+router = DefaultRouter()
+router.register(r'villagers', VillagerViewSet)
+router.register(r'drivers', DriverViewSet)
+router.register(r'leaders', LeaderViewSet)
+router.register(r'sellers', SellerViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),  # قرار دادن Router در URL با prefix api/
+]
